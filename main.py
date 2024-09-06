0669b51b428794f034c4f0e8e39162dc76033214
User <1@1.ru>
2024-09-06 19:25:16 +0400

    2-й коммит по проекту

--- main.py
+++ main.py
@@ -3,16 +3,36 @@ help - напечатать справку о программе
 add - добавить задачу в список (имя задачи будет запрашиваться)
 show - напечатать все добавленные задачи'''
 tasks = []
+today=[]
+tomorrow = []
+other =[]
 
-command = input('Введите команду: ')
-if command=='help':
-    print(HELP)
-elif command=='add':
-    task = input('Введите название задания: ')
-    tasks.append(task)
-    print('Задача добавлена')
-    print(tasks)
-elif command=='show':
-    print(tasks)
-else:
-    print('Неизвестная команда')
+while True:
+    commandos = input('Введите команду: ')
+    if commandos=='help':
+       print(HELP)
+    elif commandos=='add':
+       data = input('Введите дату:')
+       task = input('Введите название задания: ')
+       if data =='Сегодня':
+           today.append(task)
+           print('Задача добавлена')
+       elif data =='Завтра':
+           tomorrow.append(task)
+           print('Задача добавлена')
+       else:
+           other.append(task)
+           print('Задача добавлена')
+    elif commandos=='show':
+        print('Сегодня')
+        print(today)
+        print('Завтра')
+        print(tomorrow)
+        print('Другие')
+        print(other)
+    elif commandos=='exit':
+        print('Спасибо за использование! До свидания!')
+        break
+    else:
+       print('Неизвестная команда')
+       break
